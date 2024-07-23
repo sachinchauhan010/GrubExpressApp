97325ff587d7dd4a46215b5c6fda34909f5fa3df
sachinchauhan010 <010sssachin@gmail.com>
2024-07-23 22:39:56 +0530

    auto fetch done

--- src/components/heroSec/Section3.js
+++ src/components/heroSec/Section3.js
@@ -7,8 +7,9 @@ const HeroSection3 = () => {
   const [restaurants, setRestaurants] = useState(null);
 
   const fetchRestaurant = async () => {
+    console.log("Called")
     try {
-      const response = await fetch(process.env.API_URI+'/api/distributor/get-restaurant', {
+      const response = await fetch(process.env.API_URI + '/api/distributor/get-restaurant', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -22,10 +23,7 @@ const HeroSection3 = () => {
       console.error("Error fetching restaurants:", error);
     }
   };
-
-  useEffect(() => {
-    fetchRestaurant();
-  }, []);
+  fetchRestaurant();
 
   if (restaurants === null) {
     return <Shimmer />;
@@ -33,7 +31,7 @@ const HeroSection3 = () => {
 
   return (
     <section className="text-secondary">
-    <h2 className="text-2xl font-bold mb-6 px-12">Popular Restaurants</h2>
+      <h2 className="text-2xl font-bold mb-6 px-12">Popular Restaurants</h2>
       <div className="flex flex-wrap justify-center md:mx-8 mx-2 text-wrap my-4 sm:flex-row flex-col">
         {restaurants?.map((restaurant) => (
           <Link to={`/restaurant/${restaurant?.resid}`} key={restaurant?.resid} className="lg:w-1/3 md:w-1/2 sm:w-1/1 xs:w-2/3 xs:m-auto w-[100%] flex flex-wrap flex-row box-border">
